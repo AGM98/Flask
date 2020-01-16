5962e5451d3529f6cdf398b68f3ce73f112c57dd
AGM98 <46557198+AGM98@users.noreply.github.com>
2020-01-16 09:43:13 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, World! <a href='/sida2'>sida2</a><a href='/sida3'>sida3</a>"
+
+@app.route("/sida2")
+def sida2():
+    return "thetta er sida 2"
+
+@app.route("/sida3")
+def sida3():
+    return render_template("http.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
